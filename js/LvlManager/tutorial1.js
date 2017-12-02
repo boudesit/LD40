04d54fc060391986e4cb5592943547d0bfbf0aa0
boudesit <boudesit@gmail.com>
2017-12-02 21:06:30 +0100

    test length

--- js/LvlManager/tutorial1.js
+++ js/LvlManager/tutorial1.js
@@ -3,7 +3,8 @@ function tutorial1(game) {
   this.murGroup = this.game.add.group();
 	this.plateform1 = null;
 	this.plateform2 = null;
-	this.murGauche = null;
+	this.platform3 = null;
+	this.platform4 = null;
 
   this.lvlManager = null;
   this.exit = null;
@@ -12,7 +13,7 @@ function tutorial1(game) {
 tutorial1.prototype.create = function create() {
 
 	//mur center
-	this.plateform1 = this.game.add.sprite(150, 20, 'plateform');
+	this.plateform1 = this.game.add.sprite(0, 500, 'plateform');
 	this.game.physics.enable(this.plateform1);
 	this.plateform1.physicsBodyType = Phaser.Physics.ARCADE;
 	this.plateform1.enableBody = true;
@@ -21,7 +22,7 @@ tutorial1.prototype.create = function create() {
   this.murGroup.add(this.plateform1);
 
 	//mur Droite
-	this.plateform2 = this.game.add.sprite(200, 20, 'plateform');
+	this.plateform2 = this.game.add.sprite(150, 500, 'plateform');
 	this.game.physics.enable(this.plateform2);
 	this.plateform2.physicsBodyType = Phaser.Physics.ARCADE;
 	this.plateform2.enableBody = true;
@@ -30,13 +31,22 @@ tutorial1.prototype.create = function create() {
 	this.murGroup.add(this.plateform2);
 
 	//mur Gauche
-	this.murGauche = this.game.add.sprite(0, 0, null);
-	this.game.physics.enable(this.murGauche);
-	this.murGauche.physicsBodyType = Phaser.Physics.ARCADE;
-	this.murGauche.enableBody = true;
-	this.murGauche.body.immovable = true;
-	this.murGauche.body.setSize(10, 600, 0, 0);
-	this.murGroup.add(this.murGauche);
+	this.platform3 = this.game.add.sprite(650, 500, 'plateform');
+	this.game.physics.enable(this.platform3);
+	this.platform3.physicsBodyType = Phaser.Physics.ARCADE;
+	this.platform3.enableBody = true;
+	this.platform3.body.immovable = true;
+	this.platform3.body.setSize(150, 20, 0, 0);
+	this.murGroup.add(this.platform3);
+
+	this.platform4 = this.game.add.sprite(500, 500, 'plateform');
+	this.game.physics.enable(this.platform4);
+	this.platform4.physicsBodyType = Phaser.Physics.ARCADE;
+	this.platform4.enableBody = true;
+	this.platform4.body.immovable = true;
+	this.platform4.body.setSize(150, 20, 0, 0);
+	this.murGroup.add(this.platform4);
+
 
   this.exit = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
   this.exit.onDown.add(this.lvlEnding, this);
