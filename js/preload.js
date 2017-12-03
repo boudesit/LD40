d8fc2790e5e183b7664cb12d53271aca58f7fc82
Quentin <quentindegraeve59@gmail.com>
2017-12-03 20:50:27 +0100

    preloqd afhqweh

--- js/preload.js
+++ js/preload.js
@@ -19,38 +19,27 @@ preload.prototype = {
 		this.game.load.spritesheet('burger','images/Items/item-burger.png',34,33,3);
 		this.game.load.spritesheet('vegetable','images/Items/item-legume.png',34,33,3);
 
-    this.game.load.spritesheet('hero_idle', 'images/Player/player-light-idle-30-80px.png',30,80,3);
-		// this.game.load.spritesheet('ressource_1', 'assets/img/ressource_1.png', 50, 50,2);//water
-		// this.game.load.spritesheet('ressource_2', 'assets/img/ressource_2.png', 50, 50,2); //food
-		// this.game.load.spritesheet('ressource_3', 'assets/img/ressource_3.png', 50, 50,2);//Rock
-    //
-		// this.game.load.spritesheet('warriorComputer', 'assets/img/unite1_run_enemy.png', 50, 50,3);
-		// this.game.load.spritesheet('archerComputer', 'assets/img/unite3_run_enemy1.png', 50, 50,3);
-    //
-		// this.game.load.spritesheet('warrior', 'assets/img/unite1_run.png', 50, 50,3);
-		// this.game.load.spritesheet('archer', 'assets/img/unite3_run.png', 50, 50,3);
-    //
-		// this.game.load.spritesheet('miner', 'assets/img/unite2_mine.png', 50, 50,3);
-    //
-		// this.game.load.spritesheet('spriteCastlePlay', 'assets/img/castle.png',200,200,3);
-		// this.game.load.spritesheet('spriteCastleComp', 'assets/img/castle_enemy.png',200,200,3);
-    //
-		// this.game.load.spritesheet('playerDeath', 'assets/img/unite1_death.png',50,50,4);
-		// this.game.load.spritesheet('computerDeath', 'assets/img/unite_death_enemy.png',50,50,4);
+		//le normale
+    this.game.load.spritesheet('hero_climb', 'images/Player/semi/player-semi-climb.png',30,80,3);
+		this.game.load.spritesheet('hero_idle', 'images/Player/semi/player-semi-idle.png',30,80,3);
+		this.game.load.spritesheet('hero_walk', 'images/Player/semi/player-semi-walk.png',30,80,3);
+		this.game.load.spritesheet('hero_jump', 'images/Player/semi/player-semi-jump.png',30,80,2);
 
+		//le gros cul
+		this.game.load.spritesheet('hero_break', 'images/Player/fat/player-fat-break.png',30,80,2);
+		this.game.load.spritesheet('hero_idle', 'images/Player/fat/player-fat-idle.png',40,100,3);
+		this.game.load.spritesheet('hero_walk', 'images/Player/fat/player-fat-walk.png',40,100,3);
+		this.game.load.spritesheet('hero_climb', 'images/Player/fat/player-fat-climb.png',30,80,3);
+
+		// le vieul maigre
+		this.game.load.spritesheet('hero_climb', 'images/Player/light/player-light-climb.png',30,80,3);
+		this.game.load.spritesheet('hero_idle', 'images/Player/light/player-light-idle.png',30,80,3);
+		this.game.load.spritesheet('hero_walk', 'images/Player/light/player-light-walk.png',30,80,3);
+		this.game.load.spritesheet('hero_jump', 'images/Player/light/player-light-jump.png',30,80,2);
 
 		//Image
 		this.game.load.image('background', 'images/Backgrounds/background-game.png');
 		this.game.load.image('gamewin', 'assets/img/gamewin.jpg');
-
-		// this.game.load.image('buttonWarrior', 'assets/img/icone_epee.png');
-		// this.game.load.image('buttonArcher', 'assets/img/icone_hache.png');
-    //
-		// this.game.load.image('buttonUnitWater', 'assets/img/icone_ressource_1.png');
-		// this.game.load.image('buttonUnitFood', 'assets/img/icone_ressource_2.png');
-		// this.game.load.image('buttonUnitRock', 'assets/img/icone_ressource_3.png');
-
-
 
 		//Sound
      		game.load.audio('gameSound', 'assets/sound/game.mp3');
